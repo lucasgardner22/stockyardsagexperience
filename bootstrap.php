1e1b81b1b4b736f4db5ef3b64a040ed8345a0ff1
Lucas Gardner <lucas@Lukes-MacBook-Pro.local>
2026-09-23 13:48:12 -0500

    Rebuilt scratch project

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+// Two constants inform much of Craft's ability to find resources in your project:
+define('CRAFT_BASE_PATH', __DIR__);
+define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH . '/vendor');
+
+// Load Composer's autoloader
+require_once CRAFT_VENDOR_PATH . '/autoload.php';
