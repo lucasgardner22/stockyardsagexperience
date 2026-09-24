3e2eb6456e3f31a22b4038cea8b35ae59de9a691
Lucas Gardner <lucas@Lukes-MacBook-Pro.local>
2026-09-24 15:44:05 -0500

    Work on demo twig template

--- config/vite.php
+++ config/vite.php
@@ -4,12 +4,12 @@ use craft\helpers\App;
 
 return [
     'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
-    'manifestPath' => '@webroot/dist/manifest.json',
+    'manifestPath' => '@webroot/dist/vite/manifest.json',
     'devServerPublic' => 'http://localhost:5173/',
-    'serverPublic' => App::env('PRIMARY_SITE_URL') . '@web/dist/',
+    'serverPublic' => '@web/dist/',
     'errorEntry' => '',
     'cacheKeySuffix' => '',
-    'devServerInternal' => '',
+    'devServerInternal' => 'http://localhost:5173/',
     'checkDevServer' => true,
     'includeReactRefreshShim' => false,
     'includeModulePreloadShim' => true,
