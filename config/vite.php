f63d8c7f038b0f98c084636fe73b1f742c916a7a
Lucas Gardner <lucas@Lukes-MacBook-Pro.local>
2026-09-25 10:51:37 -0500

    First template setup in CraftCMS

--- config/vite.php
+++ config/vite.php
@@ -4,7 +4,7 @@ use craft\helpers\App;
 
 return [
     'useDevServer' => App::env('ENVIRONMENT') === 'dev' || App::env('CRAFT_ENVIRONMENT') === 'dev',
-    'manifestPath' => '@webroot/dist/vite/manifest.json',
+    'manifestPath' => '@webroot/dist/.vite/manifest.json',
     'devServerPublic' => 'http://localhost:5173/',
     'serverPublic' => '@web/dist/',
     'errorEntry' => '',
